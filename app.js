1cf97e4b3489464df7a54997f56bbc632b93bb1b
EL <joselezama.emilio@gmail.com>
2019-07-16 13:13:07 +0200

    Mes y tabla creados

---
+++ app.js
@@ -0,0 +1,9 @@
+//https://devcode.la/tutoriales/calendario-con-javascript/
+
+//Creamos el contenedor de Mes
+
+let mes = document.createElement("DIV");
+mes.classNaem = "mes";
+document.body.appendChild(mes);
+
+//Creamos la tabla, estructura del Mes
