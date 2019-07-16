afe8def2a1c84f0099a405d4a1322e3052b0abea
EL <joselezama.emilio@gmail.com>
2019-07-16 13:23:14 +0200

    Nombre de meses

--- app.js
+++ app.js
@@ -7,3 +7,27 @@ mes.classNaem = "mes";
 document.body.appendChild(mes);
 
 //Creamos la tabla, estructura del Mes
+
+let tabla_mes = document.createElement("TABLE");
+tabla_mes.className = "tabla_mes";
+mes.appendChild(tabla_mes);
+
+//Titulo o nombre de Mes
+
+let titulo = document.createElement("CAPTION");
+titulo.className = "titulo";
+titulo.innerText = "Enero";
+tabla_mes.appendChild(titulo);
+
+//Titulos Meses
+
+var mes_text = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+//La función nombrará cada mes
+
+function estructurar(){
+    for(m = 0; m <=11; m++){
+        //Titulo
+        titulo.innerText = mes_text[m];
+    }
+}
